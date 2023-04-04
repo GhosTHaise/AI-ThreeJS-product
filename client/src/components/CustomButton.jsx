afe0075f5c6c93cc85fd35fe4f47724c6d5c1258
GhosTHaise <Ghostrex2@gmail.com>
2023-04-04 21:06:12 +0300

    Initi Customizer page

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import state from '../store';
+import { useSnapshot } from 'valtio';
 
-const CustomButton = ({title,type,handleClick}) => {
-  return (
+const CustomButton = ({title,type,handleClick,customStyles}) => {
+    
+    const snap = useSnapshot(state);
+
+    const generateStyle = (type) => {
+        if(type === "filled")
+            return {
+                    backgroundColor : snap.color,
+                    color : "#fff"
+                }
+    }
+    return (
     <button
-        className={`px-2`}
-        type={type}
+        className={`px-2 py-1.5 flex-[1] rounded-md ${customStyles}`}
+        style={generateStyle(type)}
+        onClick={handleClick}
     >
         {title}
     </button>
