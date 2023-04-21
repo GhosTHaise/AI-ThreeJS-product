3f751691d924e018002e5551ce95754184e9cfe6
GhosTHaise <Ghostrex2@gmail.com>
2023-04-21 14:39:37 +0300

    Create api picker components

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -3,10 +3,39 @@ import React from 'react'
 import CustomButton from "./CustomButton";
 
 const AiPicker = ({
-  
+  prompt,
+  setPrompt,
+  generatingImg,
+  handleSubmit
 }) => {
   return (
-    <div>AiPicker</div>
+    <div className='aipicker-container'>
+      <textarea
+        placeholder='ask ai ...'
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className='aipicker-textarea'
+      />
+      <div className='flex flex-wrap gap-3'>
+          {generatingImg ? (
+              <CustomButton
+                type="outline"
+                title="Asking AI..."
+                customStyles="text-xs"
+              />
+          ) : (
+            <>
+              <CustomButton
+                type="outline"
+                title="AI Full"
+                handleSubmit={()=> handleSubmit("full")}
+                customStyles="text-xs"
+              />
+            </>
+          )}
+      </div>
+    </div>
   )
 }
 
